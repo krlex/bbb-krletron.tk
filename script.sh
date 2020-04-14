6e18b257817295d7f01bf54ace788f90942592cf
Krstan Vjestica <krle@tilda.center>
2020-04-14 04:46:10 +0200

    add mini help for creating account

--- script.sh
+++ script.sh
@@ -7,3 +7,6 @@ git clone https://github.com/bigbluebutton/bbb-install
 cd bbb-install/
 
 ./bbb-install.sh -v xenial-220 -s bbb.krletron.tk -e krle@tilda.center -g
+
+echo "Create Admin account:"
+echo "docker exec greenlight-v2 bundle exec rake user:create["name","email","password","admin"]"
