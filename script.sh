24b291fa2682b2f3dc953d6daeaac2634a436b11
Krstan Vjestica <krle@tilda.center>
2020-05-18 22:51:58 +0200

    filtered by script check

--- script.sh
+++ script.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-USERNAME=$1
-PASSWD=$2
-SUB=$3
-DOMAIN=4
-EMAIL=$5
-
+export USERNAME=$1
+export PASSWD=$2
+export SUB=$3
+export DOMAIN=$4
+export EMAIL=$5
 
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git vim tmux
 git clone https://github.com/bigbluebutton/bbb-install
-cd bbb-install/
 
-./bbb-install.sh -v xenial-220 -s $SUB.krletron.tk -e $EMAIL -g
+
+./bbb-install/bbb-install.sh -v xenial-220 -s "$SUB"."$DOMAIN" -e "$EMAIL" -g
 
 echo "How to create Admin account:"
 echo "https://docs.bigbluebutton.org/greenlight/gl-admin.html#creating-an-administrator-account"
 
-docker exec greenlight-v2 bundle exec rake user:create["$USENAME","$EMAIL","$PASSWD","admin"]
+docker exec greenlight-v2 bundle exec rake user:create["$USERNAME","$EMAIL","$PASSWD","admin"]
