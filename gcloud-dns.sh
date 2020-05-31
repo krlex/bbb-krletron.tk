55a94eae8c74d802be23d59174cb64ebef0c0497
Krstan Vjestica <krle@tilda.center>
2020-05-31 14:54:56 +0200

    create functions for DNS

--- gcloud-dns.sh
+++ gcloud-dns.sh
@@ -5,14 +5,17 @@ export SUBNAME="class"
 export IP_ADDRESS="$(gcloud compute instances list | tail -n+2 | awk '{print $1, $5}' |  awk '/$SUBNAME/{getline;getline;print $2}')"
 export DNSZONE="krletron"
 
-export START_DNS= $(gcloud dns record-sets transaction start --zone=$DNSZONE)
-export SET_DNS= $(gcloud dns --project=$PROJECT record-sets transaction add $IP_ADDRESS --name=$SUBNET.$DOMAIN \
+config() {
+
+  gcloud dns record-sets transaction start --zone=$DNSZONE
+
+  gcloud dns --project=$PROJECT record-sets transaction add $IP_ADDRESS --name=$SUBNET.$DOMAIN \
   --ttl="30" \
   --type="A" \
-  --zone=$DNSZONE)
+  --zone=$DNSZONE
 
-export EXE_DNS= $(gcloud dns --project=$PROJECT record-sets transaction execute --zone=$DNSZONE)
+  gcloud dns --project=$PROJECT record-sets transaction execute --zone=$DNSZONE
 
-$START_DNS
-$SET_DNS
-$EXE_DNS
+}
+
+config
