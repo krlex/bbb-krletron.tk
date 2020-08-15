d3d14e652f9148ddcd49b03238018e08bb6a642b
Krstan Vjestica <krle@tilda.center>
2020-08-15 14:31:05 +0200

    set for firewall on GCP

--- gcp.tf
+++ gcp.tf
@@ -4,7 +4,7 @@ provider "google" {
    region      = "us-central1"
 }
 
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "class" {
   name         = "class"
   machine_type = "n1-standard-4"
   zone         = "us-central1-a"
@@ -15,11 +15,11 @@ resource "google_compute_instance" "default" {
   }
 }
 
-  metadata_startup_script = "sudo apt-get -y update; sudo apt-get -y dist-upgrade ;"
-  provisioner "file" {
-    source      = "./script.sh"
-    destination = "/tmp/script.sh"
-  }
+  #metadata_startup_script = "sudo apt-get -y update; sudo apt-get -y dist-upgrade ; sudo apt install nginx; sudo service nginx start"
+  #provisioner "file" {
+  #  source      = "./script.sh"
+  #  destination = "/tmp/script.sh"
+  #}
   network_interface {
   network = "default"
   access_config {
@@ -27,6 +27,39 @@ resource "google_compute_instance" "default" {
   }
 }
 
+resource "google_dns_record_set" "a" {
+  name         = "class.${google_dns_managed_zone.class.dns_name}"
+  managed_zone = google_dns_managed_zone.class.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [google_compute_instance.class.network_interface.0.access_config.0.nat_ip]
+}
+
+resource "google_dns_managed_zone" "class" {
+  name     = "class"
+  dns_name = "krletron.tk."
+}
+
+resource "google_compute_firewall" "class" {
+  name    = "class-firewall"
+  network = google_compute_network.class.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+}
+
+resource "google_compute_network" "class" {
+  name = "class-network"
+}
+
+
 output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.class.network_interface.0.access_config.0.nat_ip}"
 }
