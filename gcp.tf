00faa8a526075dca12b6f681a1f1c4ff8d7dcdfb
Krstan Vjestica <krle@tilda.center>
2020-08-15 12:05:33 +0200

    new config for startup instance on GCP

--- gcp.tf
+++ gcp.tf
@@ -1,22 +1,32 @@
 provider "google" {
-  credentials = file("tfsvc.json")
-  version = "3.34"
-  region  = "us-central1"
-  project = "testing-system-270517"
-  zone    = "us-central1-a"
+   credentials = file("tfsvc.json")
+   project     = "testing-system-270517"
+   region      = "us-central1"
 }
 
 resource "google_compute_instance" "default" {
-  name = "class"
+  name         = "class"
   machine_type = "n1-standard-4"
+  zone         = "us-central1-a"
 
   boot_disk {
-   initialize_params {
-     image = "ubuntu-1604-xenial-v20200807"
-   }
+    initialize_params {
+    image = "ubuntu-1604-xenial-v20200807"
+  }
 }
 
- network_interface {
-   network = "default"
+  metadata_startup_script = "sudo apt-get -y update; sudo apt-get -y dist-upgrade ;"
+  provisioner "file" {
+    source      = "./script.sh"
+    destination = "/tmp/script.sh"
+  }
+  network_interface {
+  network = "default"
+  access_config {
+    }
+  }
 }
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
